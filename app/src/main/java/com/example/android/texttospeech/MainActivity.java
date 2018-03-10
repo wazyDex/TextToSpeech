7cb12863055705b486f73f9e7bef0e0ab1835c07
Dex.Wasy <dexwasy@gmail.com>
2018-03-10 11:52:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/texttospeech/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.android.texttospeech;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextToSpeech t1;
+    EditText ed1;
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ed1=(EditText) findViewById(R.id.editText2);
+        b1=(Button) findViewById(R.id.button2);
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR)
+                {
+                    t1.setLanguage(Locale.CANADA);
+                }
+
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String toSpeak = ed1.getText().toString();
+                Toast.makeText(getApplicationContext(),toSpeak,Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+    }
+
+
+    public void onPause ()
+    {
+        if(t1!=null)
+        {
+            t1.stop();
+        }
+        super.onPause();
+    }
+}
